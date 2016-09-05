f27c3a80fd3335a22f8b9cf7f2f82a29f977c6e8
puzanov <puzanov@gmail.com>
2016-09-05 15:32:27 +0600

    interface auto refresh removed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -31,9 +31,6 @@ class App extends React.Component {
              fetch('/dashboard/v1/pages')
                          .then((response) => response.json())
                          .then(self.receiveResponse)
-                         .then(function (){
-                            setTimeout(self._grabInfo, INTERVAL);
-                         })
                          .catch((ex) => {
                              console.error(ex);
                              setTimeout(self._grabInfo, INTERVAL);
