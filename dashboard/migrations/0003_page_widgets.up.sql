12dd27339e0b6ddfec2b964433c3860de36a79f0
cyberlight <cyberlight@live.ru>
2016-10-14 10:50:10 +0600

    SM-6 #time 15m Changed page widgets migration added foreign keys

--- dashboard/migrations/0003_page_widgets.up.sql
+++ dashboard/migrations/0003_page_widgets.up.sql
@@ -2,4 +2,6 @@ CREATE TABLE IF NOT EXISTS `page_widgets` (
 	`id_widget`	INTEGER NOT NULL,
 	`id_page`	INTEGER NOT NULL,
 	PRIMARY KEY(id_widget,id_page)
+  FOREIGN KEY (id_widget) REFERENCES widgets(id)
+	FOREIGN KEY (id_page) REFERENCES pages(id)
 );
